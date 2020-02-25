95b274a94fe1b4659331baba82e1a3d1b100aca9
Selmir Mujezinovic <32566835+SelmirMujezinovic@users.noreply.github.com>
2020-02-25 10:34:09 +0100

    Create Weather Component and add route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {allAppRoutes} from './routes';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(allAppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
