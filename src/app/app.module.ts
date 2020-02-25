21a4d5c0c7862075227c3e7ae1b60a2b69dea0ec
Selmir Mujezinovic <32566835+SelmirMujezinovic@users.noreply.github.com>
2020-02-25 14:24:20 +0100

    Wire Form and Update spec.ts files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 import {allAppRoutes} from './routes';
-
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 
@@ -13,7 +14,10 @@ import { WeatherComponent } from './weather/weather.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(allAppRoutes)
+    RouterModule.forRoot(allAppRoutes),
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
